af00cd26df005b874d4e5639bcb05c5d5f0dc580
jpeg25 <jairo25509@hotmail.com>
2021-09-17 21:30:16 -0500

    Day 2 started.

---
+++ Day-2/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end();
+});
+
+server.listen(8081, () => {
+  console.log("Server listening on port 8081...");
+});
+console.log("object");
+/*Por hacer. */
