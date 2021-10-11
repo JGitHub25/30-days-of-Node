0861446c145a0f6f6be42db444d7b04bd803e7a7
jpeg25 <jairo25509@hotmail.com>
2021-10-11 11:04:12 -0500

    Alternative routing option.

--- Day-5/routes/products.js
+++ Day-5/routes/products.js
@@ -8,9 +8,12 @@ const {
   deleteProduct,
 } = require("../controllers/products");
 
-router.get("/", getProducts);
-router.post("/", postProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+// router.get("/", getProducts);
+// router.post("/", postProduct);
+// router.put("/:id", updateProduct);
+// router.delete("/:id", deleteProduct);
+
+router.route("/").get(getProducts).post(postProduct);
+router.route("/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
