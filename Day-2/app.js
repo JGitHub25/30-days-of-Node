d349256206910aae6e4ea0e1cbd7b8fd0f8ef828
jpeg25 <jairo25509@hotmail.com>
2021-10-20 09:27:25 -0500

    Adjustments in Day 1, Day 2 elaborated but not working as desired.

--- Day-2/app.js
+++ Day-2/app.js
@@ -1,13 +1,53 @@
+//!ESTA VUELTA -EL EVENTO- NO ME FUNCIONA COMO QUISIERA.
+
 const events = require("events");
-const myEmitter = new events.EventEmitter();
 const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   res.end();
-// });
+//
+let people = [
+  "Johnatan",
+  "Gary",
+  "Melinda",
+  "Peter",
+  "Elsa",
+  "Ryan",
+  "Paul",
+  "Susan",
+  "Elizabeth",
+  "Gloria",
+];
+let users = ["Johnatan", "Gary", "Melinda", "Peter", "Elsa"];
 
-// server.listen(8081, () => {
-//   console.log("Server listening on port 8081...");
-// });
-console.log("object");
-/*Por hacer. */
+const randomUser = people[Math.floor(Math.random() * 11)];
+console.log(randomUser);
+
+//Event.
+const myEmitter = new events.EventEmitter();
+function giveAccess(user) {
+  let message = `Welcome ${user}`;
+  console.log(message);
+  return message;
+}
+function blockAccess(user) {
+  let message = `Sorry ${user}, your account doesn't seem to exist.`;
+  console.log(message);
+  return message;
+}
+
+if (users.includes(randomUser)) {
+  myEmitter.on("known user", giveAccess);
+  myEmitter.emit("known user", randomUser);
+} else {
+  myEmitter.on("failed attempt", blockAccess);
+  myEmitter.emit("failed attempt", "stranger");
+}
+
+//Create server.
+const server = http.createServer((req, res) => {
+  res.end();
+});
+
+//Start server.
+server.listen(8081, () => {
+  console.log("Server listening on port 8081...");
+});
