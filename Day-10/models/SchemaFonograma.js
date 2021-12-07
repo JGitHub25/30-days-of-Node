c0e22490dafb67c80c05a492484754c06541fef0
jpeg25 <jairo25509@hotmail.com>
2021-12-07 16:19:46 -0500

    timestamps included in Schema.

--- Day-10/models/SchemaFonograma.js
+++ Day-10/models/SchemaFonograma.js
@@ -1,39 +1,42 @@
 const mongoose = require("mongoose");
 
-const SchemaFonograma = new mongoose.Schema({
-  artista: {
-    type: String,
-    required: [true, "Por favor ingrese un artista."],
-  },
-  cod_proy: {
-    type: String,
-    required: [true, "Por favor ingrese un código para el proyecto."],
-  },
-  album: {
-    type: String,
-    required: [true, "Por favor ingrese un álbum."],
-  },
-  nombre: {
-    type: String,
-    required: [true, "Por favor ingrese el nombre del fonograma(canción)."],
-  },
-  categoria: {
-    type: String,
-    enum: {
-      values: ["Álbum", "EP", "Single"],
-      message:
-        "{VALUE} no es una opción válida. Por favor escoja: Álbum, EP o Single.",
+const SchemaFonograma = new mongoose.Schema(
+  {
+    artista: {
+      type: String,
+      required: [true, "Por favor ingrese un artista."],
+    },
+    cod_proy: {
+      type: String,
+      required: [true, "Por favor ingrese un código para el proyecto."],
+    },
+    album: {
+      type: String,
+      required: [true, "Por favor ingrese un álbum."],
+    },
+    nombre: {
+      type: String,
+      required: [true, "Por favor ingrese el nombre del fonograma(canción)."],
+    },
+    categoria: {
+      type: String,
+      enum: {
+        values: ["Álbum", "EP", "Single"],
+        message:
+          "{VALUE} no es una opción válida. Por favor escoja: Álbum, EP o Single.",
+      },
+    },
+    sayco: {
+      type: Boolean,
+      default: false,
+    },
+    acinpro: {
+      type: Boolean,
+      default: false,
     },
   },
-  sayco: {
-    type: Boolean,
-    default: false,
-  },
-  acinpro: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 const FonogramaModel = mongoose.model("Fonograma", SchemaFonograma);
 
