ee92005e9a6c1029c3a4a4cbe8343b95a354e0ad
jpeg25 <jairo25509@hotmail.com>
2021-12-08 09:33:59 -0500

    Security packages added.

--- Day-10/app.js
+++ Day-10/app.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv").config();
 require("express-async-errors");
 const { StatusCodes } = require("http-status-codes");
 
+//Security packages.
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+
 //db
 const connectDB = require("./db/connect");
 
@@ -15,6 +21,19 @@ const fonoRouter = require("./routes/fonoRoutes");
 
 //JSON
 app.use(express.json());
+
+//Security packages.
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //TEST ROUTE.
 // app.get("/", (req, res) => {
